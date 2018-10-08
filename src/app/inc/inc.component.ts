1e8ef42a4d6fec8615e3b7a7e2c3d12f2c8db322
debiancc <wtf@debiancc.me>
2018-10-08 23:08:40 +0800

    [feat]: add first simple component - inc

---
+++ src/app/inc/inc.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-inc',
+  templateUrl: './inc.component.html',
+  styleUrls: ['./inc.component.css']
+})
+export class IncComponent implements OnInit {
+
+  constructor() {
+  }
+
+  num = 0;
+
+  ngOnInit() {
+  }
+
+  handleAddButtonOnClick() {
+    this.num++;
+  }
+
+}
