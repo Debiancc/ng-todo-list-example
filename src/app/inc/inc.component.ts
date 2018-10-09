0247dbb0759849b916be59d291c7ec6234857771
debiancc <wtf@debiancc.me>
2018-10-09 14:11:13 +0800

    [feat]: Add inc operation log list

--- src/app/inc/inc.component.ts
+++ src/app/inc/inc.component.ts
@@ -1,5 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 
+interface IncOpLog {
+  time: Date;
+  value: number;
+  operationType: 'plus';
+}
+
 @Component({
   selector: 'app-inc',
   templateUrl: './inc.component.html',
@@ -14,11 +20,18 @@ export class IncComponent implements OnInit {
 
   incStep = 1;
 
+  incOpLogs: IncOpLog[] = [];
+
   ngOnInit() {
   }
 
   handleAddButtonOnClick() {
     this.num = this.num + this.incStep;
+    this.incOpLogs.push({time: new Date(), value: this.incStep, operationType: 'plus'});
+  }
+
+  handleRevertButtonOnClick(opLog: IncOpLog) {
+    this.num = this.num - opLog.value;
   }
 
 }
